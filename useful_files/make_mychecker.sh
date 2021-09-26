a651e430c52c4192f17d0c1f5e989a39af303340
Jaka Murovec banic <jmurovec@f0r3s4.codam.nl>
2021-09-26 19:28:56 +0200

    ready

---
+++ useful_files/make_mychecker.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+cd mychecker_bonus
+# make fclean
+make
+cd ..
---
+++ useful_files/norm.sh
@@ -0,0 +1,47 @@
+
+# PUSH SWAP
+norminette push_swap.c 
+norminette utils.c 
+norminette initial_check.c
+norminette operations_push.c
+norminette operations_swap.c 
+norminette operations_rotate.c 
+norminette sort_3_elements.c 
+norminette sort_5_elements.c 
+norminette sort_5plus_elements.c 
+norminette sort_and_simplify.c
+norminette push_swap.h
+
+# PUSH SWAP LIBFT
+cd libft
+norminette ft_atoi.c
+norminette ft_isdigit.c
+norminette ft_putstr.c
+norminette ft_strlen.c
+norminette libft.h
+
+# MYCHECKER BONUS
+cd ..
+cd mychecker_bonus
+norminette mychecker_bonus.c 
+norminette initial_check.c 
+norminette initial_check_utils.c 
+norminette final_check.c 
+norminette operations_push.c
+norminette operations_swap.c 
+norminette operations_rotate.c 
+norminette sort_3_elements.c 
+norminette sort_5_elements.c 
+norminette sort_5plus_elements.c 
+norminette sort_and_simplify.c
+norminette mychecker_bonus.h
+
+
+# PUSH SWAP LIBFT
+cd libft
+norminette ft_atoi.c
+norminette ft_isdigit.c
+norminette ft_putstr.c
+norminette ft_strlen.c
+norminette libft.h
+cd ../..
